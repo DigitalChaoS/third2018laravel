67d323d02c86229a8c1ac1059f8f17cde97c17d4
Christian Guerra <christianguerra87@gmail.com>
2018-07-10 12:01:51 -0400

    Creating and add questions to questions table.

--- database/migrations/2018_07_10_153421_create_questions_table.php
+++ database/migrations/2018_07_10_153421_create_questions_table.php
@@ -15,6 +15,8 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->longText('body');
             $table->timestamps();
         });
     }
