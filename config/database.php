c0c6a46e26957258a16185ecdfe120bba8d0e594
Christian Guerra <christianguerra87@gmail.com>
2018-07-24 15:35:07 -0400

    Procfile for Heroku

--- config/database.php
+++ config/database.php
@@ -77,7 +77,7 @@ return [
     |
     | This table keeps track of all the migrations that have already run for
     | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
+    | the migrations on disk haven't actually been run in the database.  This is a change for test.
     |
     */
     'migrations' => 'migrations',
